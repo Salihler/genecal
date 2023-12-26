08a94916e8da169e07cc47b9b7eaef242697f083
erensalihler <erensalihler@TCMX-YZLM-ES>
2023-12-26 17:15:10 +0300

    .

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+function doSomething() {
+    let generalIncome = document.getElementById("generalIncome");
+    let goal = document.getElementById("generalIncome");
+    
+    if (Number(generalIncome.innerHTML) == Number(goal.innerHTML))
+    {
+        generalIncome.innerHTML = Number(generalIncome.innerHTML) + 1;
+    }
+    else
+    alert("Bitti");
+}
 
+window.onload = function () {
+    setInterval(doSomething, 1000);
+}
